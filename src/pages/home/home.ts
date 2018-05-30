2d5790313ea0ee1daccbf0fd979cc4fe194bdac6
safefog <safefog@daum.net>
2018-05-30 21:17:20 +0900

    11111

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {ModalController, NavController} from 'ionic-angular';
+import {Profile} from "../../interface/profile";
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,27 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public profile = {} as Profile;
+  constructor(public navCtrl: NavController,
+              public modalCtrl:ModalController) {
 
   }
+
+  modal(){
+    let modal = this.modalCtrl.create('ModalPage');
+    modal.onDidDismiss(data => {
+      if(Object.keys(data).length ===0){
+alert('11');
+      } else {
+        this.profile.actionSwitch = data.actionSwitch;
+        this.profile.name = data.name;
+        this.profile.gender = data.gender;
+        this.profile.domestic = data.domestic;
+        this.profile.startDate = data.startDate;
+      }
+    });
+    modal.present();
+  }
+
 
 }
